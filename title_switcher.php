880159c2739894f0d4993e9ba6a675054bdb42bb
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-01-10 11:30:21 -0800

    initial commit

---
+++ title_switcher.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: Title Tag Switcher
+* Description: Plugin that lets you update your pages tiltle tags.
+* Author: CJR
+* Author URL: cjramirez.tech
+* Version: 1.0.0
+* Text Domain: title_switcher
+*/
+
+//Exists if accessed directly
+defined('ABSPATH') or die('Get out of here!');
