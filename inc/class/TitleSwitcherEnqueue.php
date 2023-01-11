4e177ea01a61003a9ad5a0081d3b783e816f3edb
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-01-10 19:51:24 -0800

    Added to wp admin menu

--- inc/class/TitleSwitcherEnqueue.php
+++ inc/class/TitleSwitcherEnqueue.php
@@ -6,11 +6,19 @@ class TitleSwitcherEnqueue
     {
         //Calls the enqueue function
         add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+
+        //Calls the adminMenu
+        add_action('admin_menu', array($this, 'adminMenu'));
     }
 
     public function enqueue()
     {
         wp_enqueue_style('title_switcher_styles', TITLE_SWITCHER_URL . 'assets/styles.css');
         wp_enqueue_script('title_switcher_scripts', TITLE_SWITCHER_URL . 'assets/scripts.js');
+    }
+
+    public function adminMenu() 
+    {
+        add_menu_page('Title Switcher', "Title Switcher", 'manage_options', 'title_switcher_menu', array(), 'dashicons-laptop', 8);
     }
 }
