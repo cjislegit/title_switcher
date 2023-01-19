ddbadccb355e731a5746abb534c4b01e6aeda3dd
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-01-18 19:43:48 -0800

    Created template for adding tags

--- templates/title_switcher_menu.php
+++ templates/title_switcher_menu.php
@@ -12,6 +12,22 @@
     <title>Title Switcher</title>
 </head>
 <body>
-    
+    <div class="container d-flex flex-column align-items-center">
+        <table class="table w-50">
+            <thead>
+                <tr>
+                    <th class="col w-50">Page</th>
+                    <th class="col w-50">Title Tag</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr>
+                    <td class="col">Home</td>
+                    <td class="col"><input type="text" name="" id=""></td>
+                </tr>
+            </tbody>
+        </table>
+        <input type="button" class="btn btn-primary btn-sm d-block" value="Update Title Tags">
+    </div>
 </body>
 </html>
