b4286797bdb65cae956801fdf39788e187a22f17
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-02-09 19:21:18 -0800

    Renamed all the global vars

--- title_switcher.php
+++ title_switcher.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
-* Plugin Name: Title Tag Switcher
+* Plugin Name: Title Tag Switcher AI
 * Description: Plugin that lets you update your pages tiltle tags.
 * Author: CJR
 * Author URL: cjramirez.tech
 * Version: 1.0.0
-* Text Domain: title_switcher
+* Text Domain: title_switcher_ai
 */
 
 //Exists if accessed directly
@@ -14,17 +14,17 @@ defined('ABSPATH') or die('Get out of here!');
 
 //Constatns should have a unique name so they don't interfere with other plugins
 //Sets the plugin path constant to the path of of the plugin
-define('TITLE_SWITCHER_PATH', plugin_dir_path(__FILE__));
+define('TITLE_SWITCHER_AI_PATH', plugin_dir_path(__FILE__));
 
 //Sets the plugin url contsant to the url of the plugin
-define('TITLE_SWITCHER_URL', plugin_dir_url(__FILE__));
+define('TITLE_SWITCHER_AI_URL', plugin_dir_url(__FILE__));
 
 //Sets the pligin const to the name of the plugin
-define('TITLE_SWITCHER_SUBS', plugin_basename(__FILE__));
+define('TITLE_SWITCHER_AI_SUBS', plugin_basename(__FILE__));
 
 //Load Classes
-require_once(TITLE_SWITCHER_PATH . 'inc/class/TitleSwitcherEnqueue.php');
-require_once(TITLE_SWITCHER_PATH . 'inc/class/TitleSwitcher.php');
+require_once(TITLE_SWITCHER_AI_PATH . 'inc/class/TitleSwitcherEnqueue.php');
+require_once(TITLE_SWITCHER_AI_PATH . 'inc/class/TitleSwitcher.php');
 
 //Create new instance of class
 $newEnqueue = new TitleSwitcherEnqueue();
